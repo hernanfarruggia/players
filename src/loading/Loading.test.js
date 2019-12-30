30ce8b95497878220a905e185df64b193d996787
Hernan Faarruggia <hernan.farruggia@gmail.com>
2019-12-30 01:18:35 -0300

    - Added Loading Component tests

---
+++ src/loading/Loading.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Loading from './index';
+
+
+describe('Loading Component', () => {
+
+    it('should render the component and display loading message', () => {
+        const Component = shallow(<Loading />);
+        expect(Component.find('div').length).toBe(1);
+        expect(Component.find('div').text()).toEqual('Loading players...');
+    });
+
+});
